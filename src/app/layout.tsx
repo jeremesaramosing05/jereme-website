8a6281384fe786e3443cf7be576778f2ceae4520
Jereme Saramosing <jeremesaramosing05@gmail.com>
2026-06-10 08:35:25 +0800

    fix: suppress expected hydration warning from next-themes on <html>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -42,6 +42,7 @@ export default function RootLayout({
     <html
       lang="en"
       className={`${geistSans.variable} ${fraunces.variable} h-full antialiased`}
+      suppressHydrationWarning
     >
       <body className="flex min-h-screen flex-col">
         <ThemeProvider>
