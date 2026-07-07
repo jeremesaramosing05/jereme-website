edc824b0103ff3ce47c9471b47dd8aa96138a67a
Jereme <jeremesaramosing05@gmail.com>
2026-07-08 07:26:22 +0800

    Update data dashboard demo: unpivot auto-suggestion

--- public/demos/data-dashboard/assets/interactiveHtml-D8hY_YeH.js
+++ public/demos/data-dashboard/assets/interactiveHtml-D8hY_YeH.js
@@ -1,4 +1,4 @@
-import{g as w,s as S,b as M,t as I,a as y,d as T,h as C,f as A,c as D}from"./index-L0VjXmUz.js";const k=`
+import{g as w,s as S,b as M,t as I,a as y,d as T,h as C,f as A,c as D}from"./index-BnPsjFoX.js";const k=`
 /*
 * Licensed to the Apache Software Foundation (ASF) under one
 * or more contributor license agreements.  See the NOTICE file
