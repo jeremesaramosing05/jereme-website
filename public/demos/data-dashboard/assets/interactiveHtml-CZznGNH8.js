f3ad8dd1000378ab1496a6e6d40a38c951a7ba1d
Jereme <jeremesaramosing05@gmail.com>
2026-07-09 13:59:16 +0800

    Update data dashboard demo: View-the-rows drill-through

--- public/demos/data-dashboard/assets/interactiveHtml-CZznGNH8.js
+++ public/demos/data-dashboard/assets/interactiveHtml-CZznGNH8.js
@@ -1,4 +1,4 @@
-import{g as w,s as S,b as M,t as I,a as y,d as T,h as C,f as A,c as D}from"./index-Cw5--qdL.js";const k=`
+import{g as w,s as S,b as M,t as I,a as y,d as T,h as C,f as A,c as D}from"./index-CBdi8uNC.js";const k=`
 /*
 * Licensed to the Apache Software Foundation (ASF) under one
 * or more contributor license agreements.  See the NOTICE file
